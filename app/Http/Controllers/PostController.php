82bc2a75df87123b73da5e2c4c102e8e84583f21
sebas1705sanchez <151795806+sebas1705sanchez@users.noreply.github.com>
2024-07-16 18:46:08 -0500

    eliminar

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        return view('posts.index', ['posts' => Post::latest()->paginate()]);
+    }
+
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return redirect()->route('posts.index');
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,7 +9,35 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    listado de publicaciones
+                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        @forelse ($posts as $post)
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {{ $post->id }}
+                                </th>
+                                <td class="py-4 px-6">
+                                    {{ $post->title }}
+                                </td>
+                                <td class="py-4 px-6">
+                                    <a href="" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar</a>
+                                </td>
+                                <td class="py-4 px-6">
+                                    <form action="{{ route('posts.destroy', $post) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="font-medium text-red-600 dark:text-red-500 hover:underline">Eliminar</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <td class="py-4 px-6">
+                                    No posts
+                                </td>
+                            </tr>
+                        @endforelse
+                    </table>
+                    {{ $posts->links() }}
                 </div>
             </div>
         </div>
