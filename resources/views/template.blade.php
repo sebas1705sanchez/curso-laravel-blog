6c04a6eb105ca348144de2b6563a4953c5871732
sebas1705sanchez <151795806+sebas1705sanchez@users.noreply.github.com>
2024-07-17 18:11:39 -0500

    diseño web

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -4,20 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Proyecto web</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <p>
-        <a href="{{ route('home') }}">Home</a>
-        <a href="{{ route('blog') }}">Blog</a>
+	<div class="container px-4 mx-auto">
+		<header class="flex justify-between items-center py-4">
+			<div class="flex items-center flex-grow gap-4">
+				<a href="{{ route('home') }}">
+					<img src="{{ asset('images/logo.png') }}" class="h-12">
+				</a>
+				<form action="">
+					<input type="text" placeholder="Buscar">
+				</form>
+			</div>
 
-        @auth
-          <a href="{{ route('dashboard') }}">Dashboard</a>
-        @else
-          <a href="{{ route('login') }}">Login</a>
-        @endauth
-    </p>
-    <hr>
+			@auth
+			<a href="{{ route('dashboard') }}">Dashboard</a>
+			@else
+			<a href="{{ route('login') }}">Login</a>
+			@endif
+		</header>
 
-    @yield('content')
+		@yield('content')
+	</div>
 </body>
 </html>
