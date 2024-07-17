4e964503f931aed1385681c83b5b7fb25bd761c0
sebas1705sanchez <151795806+sebas1705sanchez@users.noreply.github.com>
2024-07-16 20:25:28 -0500

    agregar

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
@@ -16,6 +17,16 @@ class PostController extends Controller
     public function create(Post $post)
     {
         return view('posts.create', ['post' => $post]);
+    }
+
+    public function store(Request $request)
+    {
+        $post = $request->user()->posts()->create([
+            'title' => $titulo = $request->title,
+            'slug' => Str::slug($titulo),
+            'body' => $request->body
+        ]);
+        return redirect()->route('posts.edit', $post);
     }
 
     public function edit(Post $post)
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,6 +9,13 @@ class Post extends Model
 {
     use HasFactory;
 
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'body',
+    ];
+
     public function user()
     {
     	return $this->belongsTo(User::class);
