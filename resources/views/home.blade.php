0b152fb536e6edd85753372ea8fe285b9693bded
sebas1705sanchez <151795806+sebas1705sanchez@users.noreply.github.com>
2024-07-18 10:38:19 -0500

    publicaciones individuales

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
        <h1 class="text-3xl mb-8 text-gray-900">Contenido tecnico</h1>
        <div class="grid grid-cols-1 gap-4 mb-4">
            @foreach($posts as $post)
-            <a href="" class="bg-gray-100 rounded-lg px-6 py-4">
+            <a href="{{ route('post', $post->slug)}}" class="bg-gray-100 rounded-lg px-6 py-4">
                 <p class="text-gray-500 text-xs flex items-center gap-2">
                     <span class="font-bold">Tutorial</span>
                     {{ $post->created_at->format('d/m/Y') }}
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,8 +1,12 @@
 @extends('template')
 
 @section('content')
-        <h1>{{ $post->title }}</h1>
-        <p>{{ $post->body }}</p>
+    <div class="p-6 text-gray-900 dark:text-gray-100 align-center">
+        <h1 class="text-5xl font-bold mb-8">{{ $post->title }}</h1>
+    </div>
+    <div class="p-6 text-gray-900 dark:text-gray-100">
+        <p class="leading-loose text-gray-700">{!! $post->body !!}</p>
+    </div>
 @endsection
 
 
