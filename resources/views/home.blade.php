829fa569c3544f4a619a8f36fc1d5d0880a94ad5
sebas1705sanchez <151795806+sebas1705sanchez@users.noreply.github.com>
2024-07-18 10:31:02 -0500

    destacado

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,11 @@
 
 @section('content')
 
-   <div>
-       <h1>Home</h1>
+   <div class="bg-gray-400 px-20 py-16 rounded-lg mb-8 relative overflow-hidden">
+       <samp class="text-3xl font-bold">Programacion</samp>
+       <h1 class="text-3xl font-bold mt-4">Blog</h1>
+       <p class="text-sm text-gray-700">Proyecto de desarrollo web para prodesionales</p>
+       <img src="{{ asset('images/dev.png') }}" class="absolute right-0 -bottom-20 opacity-60" >
    </div>
    <div>
        <h1 class="text-3xl mb-8 text-gray-900">Contenido tecnico</h1>
